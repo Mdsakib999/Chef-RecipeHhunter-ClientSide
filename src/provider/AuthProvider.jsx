73b502898e4aebfac066f9525c57ef6aa525427a
Mdsakib999 <sakibabrar999@gmail.com>
2023-05-04 23:18:58 +0600

    some change

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -14,12 +14,15 @@ const auth = getAuth(app);
 const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
 
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const signIn = (email, password) =>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
@@ -31,6 +34,7 @@ const AuthProvider = ({children}) => {
         const unsubscribe = onAuthStateChanged(auth, logUser =>{
             console.log(logUser);
             setUser(logUser);
+            setLoading(false)
         })
 
         return () => {
@@ -40,6 +44,7 @@ const AuthProvider = ({children}) => {
 
     const authInfo = {
             user,
+            loading,
             createUser,
             signIn,
             logOut
--- src/routs/PrivateRoute.jsx
+++ src/routs/PrivateRoute.jsx
@@ -1,10 +1,15 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import { Spinner } from "flowbite-react";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
+  if(loading) {
+    return <Spinner className="text-center" aria-label="Default status example" />
+
+  }
 
   if (user) {
     return children;
